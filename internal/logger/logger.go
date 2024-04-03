4a0362c5e69874e40aadb3545f94e407dc845078
h3h3 <h3h3.one@gmail.com>
2024-04-03 11:46:04 +0300

    edit log

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"io"
+	"log"
 	"log/slog"
 	"os"
 )
@@ -13,18 +15,19 @@ const (
 
 func New(env string) {
 	var logger *slog.Logger
-	f, err := os.OpenFile("/root/springoff/logs/file.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return
-	}
+
+	file, _ := os.OpenFile("/root/springoff/logs/file.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	iw := io.MultiWriter(os.Stdout, file)
+	log.SetOutput(iw)
 
 	switch env {
 	case envLocal:
-		logger = slog.New(slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelInfo}))
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	case envDev:
-		logger = slog.New(slog.NewJSONHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
-		logger = slog.New(slog.NewJSONHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	}
+
 	slog.SetDefault(logger)
 }
